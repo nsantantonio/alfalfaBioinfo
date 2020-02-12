e351838713bbba20a1e7118f01aa9a02fae20e54
Nicholas Santantonio <ns722@cornell.edu>
2020-02-12 18:19:29 -0500

    ran this to make bams

--- makeBams.sh
+++ makeBams.sh
@@ -1,8 +1,9 @@
-nC=40
+nC=50
 mapq=20
-# genome="/local/rdata/dbgap_robbins/alfalfa3a.2/3a.2-bionano+unscaffolded-assembly.fa"
-# using just the first 978 scaffolds, as they appear to be the main genome sequence. Wrote Andrew Farmer email to confirm
-genome="/local/rdata/dbgap_robbins/alfalfaMainGenome/3a.2-bionano+unscaffolded-assembly_1to978.fa"
+genome="/local/rdata/dbgap_robbins/alfalfa3a.2/3a.2-bionano+unscaffolded-assembly.fa"
+# using just the first 978 scaffolds, as they appear to be the main genome sequence. Wrote Andrew Farmer email to confirm.
+# This is currently unclear
+# genome="/local/rdata/dbgap_robbins/alfalfaMainGenome/3a.2-bionano+unscaffolded-assembly_1to978.fa"
 workDir=alfalfaWorkdir
 seqDir=alfalfaNovaSeq
 bamDir=alfalfaBams
@@ -10,10 +11,10 @@ ls ${seqDir}/*.fastq.gz > ${workDir}/seqFiles.txt
 sed 's/_L001_.*\|_L002_.*\|.*\///g' ${workDir}/seqFiles.txt | uniq > ${workDir}/gidWells.txt 
 sed 's/-Rep._.*//g' ${workDir}/gidWells.txt | sed 's/.*-//g' | uniq > ${workDir}/gid.txt 
 sed 's/.*alfalfaDNAext-//g' ${workDir}/gidWells.txt | sed 's/-.*//g' | uniq > ${workDir}/wells.txt 
-cat ${workDir}/seqFiles.txt
-cat ${workDir}/gidWells.txt
-cat ${workDir}/gid.txt
-cat ${workDir}/wells.txt
+# cat ${workDir}/seqFiles.txt
+# cat ${workDir}/gidWells.txt
+# cat ${workDir}/gid.txt
+# cat ${workDir}/wells.txt
 
 while read g; do
 	bwa mem -t ${nC} ${genome} ${seqDir}/${g}_L001_R1_001.fastq.gz ${seqDir}/${g}_L001_R2_001.fastq.gz > ${bamDir}/${g}_L001_aln.sam
