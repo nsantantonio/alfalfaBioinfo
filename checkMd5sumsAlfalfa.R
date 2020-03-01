0f5f4e97ed320a40cf3770443978e1216a2771ee
Nicholas Santantonio <ns722@cornell.edu>
2020-03-01 10:47:46 -0500

    check md5sums updated to cli

--- checkMd5sumsAlfalfa.R
+++ checkMd5sumsAlfalfa.R
@@ -1,10 +1,14 @@
-novaSeq <- read.table("alfalfaNovaSeq/alfalfaNovaSeqMd5sums.txt")
-md5FromAdrien <- read.table("md5sum.source")
+args <- commandArgs(TRUE)
+print(args)
 
-if(all(md5FromAdrien[[1]] == novaSeq[[1]])) cat("md5sums match!\n") else cat("Shoot dang! you gotta problem with these ones feller:\n", novaSeq[md5FromAdrien[[1]] != novaSeq[[1]], 2])
+if(length(args)){
+	md5calc <- args[[1]]
+	md5True <- args[[2]]
 
-Robbins <- read.table("Robbins-NS-8397_2020_01_14/Robbins-NS-8397_2020_01_14Md5sums.txt")
-md5FromAdrien <- read.table("md5sum.source")
+	novaSeq <- read.table(md5calc)
+	md5FromAdrien <- read.table(md5True)
 
-if(all(md5FromAdrien[[1]] == Robbins[[1]])) cat("md5sums match!\n") else cat("Shoot dang! you gotta problem with these ones feller:\n", Robbins[md5FromAdrien[[1]] != Robbins[[1]], 2])
-
+	if(all(md5FromAdrien[[1]] == novaSeq[[1]])) cat("md5sums match!\n") else cat("Shoot dang! you gotta problem with these ones feller:\n", novaSeq[md5FromAdrien[[1]] != novaSeq[[1]], 2])
+} else {
+	stop("please provide the following:\npath to calculated md5sums\npath to true md5sums from Adrien\n")
+}
