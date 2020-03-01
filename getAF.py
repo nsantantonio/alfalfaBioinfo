de02d671c32b520a3477218a1e2902dd34daca0a
Nicholas Santantonio <ns722@cornell.edu>
2020-03-01 09:10:38 -0500

    added sys args

--- getAF.py
+++ getAF.py
@@ -4,11 +4,15 @@ import re
 # line = "##contig=<ID=scaffold941|size354645|quiver,length=355866>"
 # line = "scaffold45|size1037999|quiver   167436  .       C       A       5.76485 .       DP=13;VDB=0.78;SGB=-2.48712;RPB=0.85;MQB=0.1;MQSB=0.594978;BQB=0.9;MQ0F=0;ICB=0.5;HOB=0.5;AC=2;AN=4;DP4=4,6,0,2;MQ=55   GT:PL:AD  0/1:31,0,31:1,1  0/1:7,0,219:9,1"
 
+infile = sys.argv[1]
+outdir = sys.argv[2]
+
+
 infoCols = 8
 startIndCols = infoCols + 1
 
-freq = open("refFreqs.txt", "w")
-count = open("totCounts.txt", "w")
+freq = open(outdir + "refFreqs.txt", "w")
+count = open(outdir + "totCounts.txt", "w")
 
 def getAf(x):
 	y = x.rsplit(":",1)[1]
@@ -21,7 +25,7 @@ def getAf(x):
 		# r = str(a[0] / float(suma))
 	return r
 
-with open('alfalfaAlignCADL/chmf.ploidy.vcf') as f:
+with open(infile) as f:
 # with open('test.vcf') as f:
 	for line in f:
 		if re.match("\\#CHROM.*", line):
@@ -40,7 +44,7 @@ count.write(" ".join(names) + "\n")
 # counts = [name + "_cnt" for name in names]
 
 
-with open('alfalfaAlignCADL/chmf.ploidy.vcf') as f:
+with open(infile) as f:
 # with open('test.vcf') as f:
 	for line in f:
 		line = line.split('#', 1)[0]
