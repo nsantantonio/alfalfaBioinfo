81eaf96c14e621077bf97f629da87ff6714f8129
Nicholas Santantonio <ns722@cornell.edu>
2020-03-01 10:55:25 -0500

    use 50 cpus

--- makeVariantCalls.sh
+++ makeVariantCalls.sh
@@ -1,5 +1,5 @@
 cd /workdir/ns722/
-nC=40
+nC=50
 mapq=20
 genome="/local/rdata/dbgap_robbins/alfalfa3a.2/3a.2-bionano+unscaffolded-assembly.fa"
 # using just the first 978 scaffolds, as they appear to be the main genome sequence. Wrote Andrew Farmer email to confirm
