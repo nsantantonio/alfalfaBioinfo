9e9d2d0aa09f065a4cf26b1b15666b2172254483
Nicholas Santantonio <ns722@cornell.edu>
2020-03-02 17:58:54 -0500

    make an info file with scaff name and pos

--- getAF.py
+++ getAF.py
@@ -4,6 +4,10 @@ import re
 # line = "##contig=<ID=scaffold941|size354645|quiver,length=355866>"
 # line = "scaffold45|size1037999|quiver   167436  .       C       A       5.76485 .       DP=13;VDB=0.78;SGB=-2.48712;RPB=0.85;MQB=0.1;MQSB=0.594978;BQB=0.9;MQ0F=0;ICB=0.5;HOB=0.5;AC=2;AN=4;DP4=4,6,0,2;MQ=55   GT:PL:AD  0/1:31,0,31:1,1  0/1:7,0,219:9,1"
 
+# line = "Super-Scaffold_1        357018  .       C       T       180     .       DP=38;VDB=0.0278409;SGB=2.66165;RPB=0.67934;MQB=0.990262;MQSB=0.93828;BQB=0.714832;MQ0F=0;ICB=1;HOB=0.0798611;AC=5;AN=24;DP4=11,18,3,4;MQ=59       GT:PL:AD        ./.:0,0,0:0,0   0/0:0,3,37:1,0  0/0:0,3,37:1,0  ./.:0,0,0:0,0   ./.:0,0,0:0,0   ./.:0,0,0:0,0   0/1:51,0,65:2,1 ./.:0,0,0:0,0   ./.:0,0,0:0,0   0/0:0,3,25:1,0  0/0:0,3,37:1,0  1/1:105,9,0:0,3    ./.:0,0,0:0,0   0/1:67,6,0:0,2  ./.:0,0,0:0,0   0/0:0,3,37:1,0  ./.:0,0,0:0,0   ./.:0,0,0:0,0   0/0:0,9,92:3,0  ./.:0,0,0:0,0   0/0:0,39,255:13,0       0/1:19,0,134:5,1        ./.:0,0,0:0,0   0/0:0,3,37:1,0"
+# infile = "alfalfaVariantCalls/test.vcf"
+# outdir = ""
+
 infile = sys.argv[1]
 outdir = sys.argv[2]
 
@@ -11,6 +15,7 @@ outdir = sys.argv[2]
 infoCols = 8
 startIndCols = infoCols + 1
 
+info = open(outdir + "namePos.txt", "w")
 freq = open(outdir + "refFreqs.txt", "w")
 count = open(outdir + "totCounts.txt", "w")
 
@@ -51,10 +56,12 @@ with open(infile) as f:
 		line = line.rstrip()
 		# if not li.startswith("#"):
 		if line:
+			scafPos = line.split()[0:5]
 			cntaf = map(getAf, line.split()[startIndCols:])
 			# print(af)
 			cnt = [el[0] for el in cntaf]
 			af = [el[1] for el in cntaf]
+			info.write(" ".join(scafPos) + "\n")
 			freq.write(" ".join(af) + "\n")
 			count.write(" ".join(cnt) + "\n")
 	freq.write('\n')
